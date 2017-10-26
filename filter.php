57b89d7cddd14cbb44fb6e213c2188fe49a2ebb6
Michael Milette <michael.milette@tngconsulting.ca>
2017-10-26 01:37:49 -0400

    Added {mycoursesmenu} tag and fixed issues with {mycourses} tag.

--- filter.php
+++ filter.php
@@ -120,7 +120,7 @@ class filter_filtercodes extends moodle_text_filter {
      * Retrieves the URL for the user's profile picture, if one is available.
      *
      * @param object $user.
-     * @return intger $size: 1 (default - medium),2 (small), or 3 (large).
+     * @return string $url This is the url to the photo image file but with $1 for the size.
      */
     private function getprofilepictureurl($user) {
         if (isloggedin() && $user->picture > 0) {
@@ -240,14 +240,14 @@ class filter_filtercodes extends moodle_text_filter {
 
         if (stripos($text, '{userpicture') !== false) {
             // Tag: {userpictureurl size}. User photo URL.
-            // Sizes: 1 (medium), 2 (small), 3 (large).
+            // Sizes: 2 (small), 1 (medium), 3 (large).
             if (stripos($text, '{userpictureurl ') !== false) {
                 $url = $this->getprofilepictureurl($USER);
                 $replace['/\{userpictureurl\s+(\w+)\}/i'] = $url;
             }
 
             // Tag: {userpictureimg size}. User photo URL wrapped in HTML image tag.
-            // Sizes: 1 (medium), 2 (small), 3 (large).
+            // Sizes: 2 (small), 1 (medium), 3 (large).
             if (stripos($text, '{userpictureimg ') !== false) {
                 $url = $this->getprofilepictureurl($USER);
                 $tag = '<img src="' . $url . '" alt="' . $firstname . ' ' . $lastname . '" class="userpicture">';
@@ -271,10 +271,10 @@ class filter_filtercodes extends moodle_text_filter {
             }
         }
 
-        // Tag: {mycourses}. Just an unordered list of links to enrolled course.
-        if (stripos($text, '{mycourses}') !== false) {
-            $list = '';
+        // Tag: {mycourses} and {mycoursesmenu}.
+        if (stripos($text, '{mycourses') !== false) {
             if (isloggedin() && !isguestuser()) {
+                // Retrieve list of user's enrolled courses.
                 $sortorder = 'visible DESC';
                 // Prevent undefined $CFG->navsortmycoursessort errors.
                 if (empty($CFG->navsortmycoursessort)) {
@@ -283,17 +283,36 @@ class filter_filtercodes extends moodle_text_filter {
                 // Append the chosen sortorder.
                 $sortorder = $sortorder . ',' . $CFG->navsortmycoursessort . ' ASC';
                 $mycourses = enrol_get_my_courses('fullname,id', $sortorder);
-                if (count($mycourses)) {
+
+                // Tag: {mycourses}. An unordered list of links to enrolled course.
+                if (stripos($text, '{mycourses}') !== false) {
+                    $list = '';
                     foreach ($mycourses as $mycourse) {
                         $list .= '<li><a href="' . (new moodle_url('/course/view.php', array('id'=>$mycourse->id))) . '">' .
                                 $mycourse->fullname . '</a></li>';
                     }
+                    if (empty($list)) {
+                        $list .= '<li>' . get_string('notenrolled', 'grades') . '</li>';
+                    }
+                    $replace['/\{mycourses\}/i'] = '<ul class="mycourseslist">' . $list . '</ul>';
                 }
+                // Tag: {mycoursesmenu}. A custom menu list of enrolled course names with links.
+                if (stripos($text, '{mycoursesmenu}') !== false) {
+                    $list = '';
+                    foreach ($mycourses as $mycourse) {
+                        $list .= '-' . $mycourse->fullname . '|' . 
+                            (new moodle_url('/course/view.php', array('id'=>$mycourse->id))) . PHP_EOL;
+                    }
+                    if (empty($list)) {
+                        $list .= '-' . get_string('notenrolled', 'grades') . PHP_EOL;
+                    }
+                    $replace['/\{mycoursesmenu\}/i'] = $list;
+                }
+                unset($list);
+                unset($mycourses);
             }
-            if (empty($list)) {
-                $list .= '<li>' . get_string('notenrolled', 'grades') . '</li>';
-            }
-            $replace['/\{mycourses\}/i'] = '<ul class="mycourseslist">' . $list . '</ul>';
+            $replace['/\{mycourses\}/i'] = '';
+            $replace['/\{mycoursesmenu\}/i'] = '';
         }
 
         // Tag: {referer}.
