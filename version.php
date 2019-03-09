64063d5e858166954ba231c9a17b86dacf5b17be
Michael Milette <michael.milette@tngconsulting.ca>
2019-03-09 10:33:58 -0500

    Fixed {categories} filter code compatibility with Moodle 2.7 to 3.5.

--- version.php
+++ version.php
@@ -18,15 +18,15 @@
  * Version information for FilterCodes.
  *
  * @package    filter_filtercodes
- * @copyright  2017-2018 TNG Consulting Inc. - www.tngconsulting.ca
+ * @copyright  2017-2019 TNG Consulting Inc. - www.tngconsulting.ca
  * @author     Michael Milette
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2018112500;            // The current plugin version (Date: YYYYMMDDXX).
+$plugin->version   = 2019030900;            // The current plugin version (Date: YYYYMMDDXX).
 $plugin->requires  = 2014051200;            // Requires Moodle version 2.7.
 $plugin->component = 'filter_filtercodes';  // Full name of the plugin (used for diagnostics).
-$plugin->release   = '1.0.0';
+$plugin->release   = '1.0.1';
 $plugin->maturity = MATURITY_STABLE;
