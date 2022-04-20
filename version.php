f2a8a245fdc46f1c1fc04e892ac082bad2f8ae3b
Michael Milette <michael.milette@tngconsulting.ca>
2022-04-19 20:13:34 -0400

    FilterCodes release 2.3.0 (2022-04-19)

--- version.php
+++ version.php
@@ -25,8 +25,8 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2022041528;            // The current plugin version (Date: YYYYMMDDXX).
+$plugin->version   = 2022041900;            // The current plugin version (Date: YYYYMMDDXX).
 $plugin->requires  = 2014051200;            // Requires Moodle version 2.7 or later.
 $plugin->component = 'filter_filtercodes';  // Full name of the plugin (used for diagnostics).
-$plugin->release   = '2.2.8';
+$plugin->release   = '2.3.0';
 $plugin->maturity  = MATURITY_STABLE;
