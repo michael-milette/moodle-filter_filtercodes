783b586afe6781fc790447acf50da3ef2c9592e6
Michael Milette <michael.milette@tngconsulting.ca>
2024-10-11 16:24:33 -0400

    Release 2.6.1

--- version.php
+++ version.php
@@ -25,8 +25,8 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2024100700;            // The current plugin version (Date: YYYYMMDDXX).
+$plugin->version   = 2024100701;            // The current plugin version (Date: YYYYMMDDXX).
 $plugin->requires  = 2014051200;            // Requires Moodle version 2.7 or later.
 $plugin->component = 'filter_filtercodes';  // Full name of the plugin (used for diagnostics).
-$plugin->release   = '2.6.0';
+$plugin->release   = '2.6.1';
 $plugin->maturity  = MATURITY_STABLE;
